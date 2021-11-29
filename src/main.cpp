d3ccd9460d18ea5351fbb10d7b05211a4dc54140
vinpar <vincent@ancroft.me.uk>
2021-11-29 14:47:57 +0000

    longer on

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);              // wait for a second
+  delay(250);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(100);              // wait for a second
 Serial.println(sin(n*2*3.1416/20));
